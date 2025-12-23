a34e64d4d959d37a2d6f5bed2dd9941c7e86b07e
RobotCruzr <kimanh.220102@gmail.com>
2025-12-23 16:00:11 +0700

    Thêm tính năng mới/Sửa giao diện

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/kimanh-portfolio/',
-  base: '/',
+  base: '/kimanh-portfolio/',
+  // base: '/',
   plugins: [react()],
 });
